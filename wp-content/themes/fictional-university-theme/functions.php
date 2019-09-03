52d2cb72d6f6f9fc4c6538dcd002deedf346523a
Katy Stucolov <katy.stucolov@dreamhost.com>
2019-09-02 17:04:05 -0700

    first commit

---
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -0,0 +1,13 @@
+<?php 
+
+    function university_files() {
+        //these are css wp_enqueue_style and/or js files wp_enqueue_script
+        wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+        wp_enqueue_style('custom-google-fonts', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+        wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    }
+
+    add_action('wp_enqueue_scripts', 'university_files');
+    //add action runs the function that's wr
+    
+?>
