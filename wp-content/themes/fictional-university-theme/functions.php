ddbeac1876f767026362ca2260b986bdc9cfd4ba
Katy Stucolov <katy.stucolov@dreamhost.com>
2019-09-03 11:32:27 -0700

    Enqueue js files

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,6 +1,7 @@
 <?php 
 
     function university_files() {
+        wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
         //these are css wp_enqueue_style and/or js files wp_enqueue_script
         wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
         wp_enqueue_style('custom-google-fonts', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
