db13609a0ba183581175a418848adfb12cea58e6
Katy Stucolov <katy.stucolov@dreamhost.com>
2019-09-06 13:16:13 -0700

    Adds dynamic links from parent to child pages

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -28,15 +28,20 @@
   ?>
    
     
-    <!--
+   
     <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
       <ul class="min-list">
         <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php 
+          wp_list_pages(array(
+            'title_li' => NULL,
+            'child_of' => get_page_id $this,
+          ));
+        ?>
       </ul>
     </div>
-    -->
+   
     
     <div class="generic-content">
       <?php the_content(); ?>
